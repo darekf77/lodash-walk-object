6d0d3408f066fe39f3b0012a9da74ecef3a044c1
Dariusz Filipiak <dariusz.filipiak@igt.com>
2019-12-23 21:25:37 +0100

    v2

---
+++ environment.js
@@ -0,0 +1,11 @@
+
+const path = require('path')
+var { config } = { config: {} };
+
+config = {
+
+    domain: 'lodash-walk-object.example.domain.com',
+
+
+}
+module.exports = exports = { config };
