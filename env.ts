ee8010f17faa048c2c7b3b18562d7ab8dc55a1eb
Dariusz Filipiak <darekf77@gmail.com>
2025-04-05 15:16:56 +0200

    $$$ update $$$

---
+++ env.ts
@@ -0,0 +1,10 @@
+// @taon-ignore
+// import type { EnvOptions } from '../../../../../../lib/src';
+
+const env: Partial<any> = {
+  website: {
+    domain: 'lodash-walk-object.example.domain.com',
+    title: 'Lodash Walk Object',
+  },
+};
+export default env;
